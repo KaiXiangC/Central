eb86b15ddcbaf6a97fdb18c5c308c0d604c77641
rki568 <44421346+rki568@users.noreply.github.com>
2018-12-08 11:28:33 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,9 +24,25 @@ io.on('connection', function (socket) {
         socket.broadcast.emit('face_identification',data);
         console.log('face_identification'+data);
     });
-    socket.on('speaker_ecognition', function(data) {
-        socket.broadcast.emit('speaker_ecognition',data);
-        console.log('speaker_ecognition'+data);
+    socket.on('speaker_recognition', function(data) {
+        socket.broadcast.emit('speaker_recognition',data);
+        console.log('speaker_recognition'+data);
+    });
+    socket.on('k_speak', function(data) {
+        socket.broadcast.emit('k_speak',data);
+        console.log('k_speak'+data);
+    });
+    socket.on('p_create', function(data) {
+        socket.broadcast.emit('p_create',data);
+        console.log('p_create'+data);
+    });
+    socket.on('name_add', function(data) {
+        socket.broadcast.emit('name_add',data);
+        console.log('name_add'+data);
+    });
+    socket.on('u_chan', function(data) {
+        socket.broadcast.emit('u_chan',data);
+        console.log('u_chan'+data);
     });
     socket.on('801', function(data) {
         socket.broadcast.emit('801',data);
@@ -47,6 +63,10 @@ io.on('connection', function (socket) {
     socket.on('805', function(data) {
         socket.broadcast.emit('805',data);
         console.log("get_news:  " + data);
+    });
+    socket.on('806', function(data) {
+        socket.broadcast.emit('806',data);
+        console.log("google_serach:  " + data);
     });
 
 });
